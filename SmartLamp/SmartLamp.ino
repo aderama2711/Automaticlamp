543cbc4a25b736942bca75fec36ccb5f75534e5a
aderama2711 <ade.ramadha@gmail.com>
2021-03-11 17:22:00 +0700

    Add comment

--- SmartLamp/SmartLamp.ino
+++ SmartLamp/SmartLamp.ino
@@ -1,31 +1,31 @@
-int relay = 4;
-int pir = 2;
-int ldr = A0;
-int statuspir = 0;
-int statusldr = 0;
+int relay = 4; //inisialisasi relay sebagai 4(pin dari relay)
+int pir = 2; //inisialisasi pir sebagai 2(pin dari pir)
+int ldr = A0; //inisialisasi ldr sebagai A0(pin dari ldr)
+int statuspir = 0; //inisialisasi variabel pembacaan pir
+int statusldr = 0; //inisialisasi variabel pembacaan ldr
 
 void setup()
 {
-  pinMode(relay, OUTPUT);
-  pinMode(pir, INPUT);
-  pinMode(ldr, INPUT);
-  Serial.begin(9600);
+  pinMode(relay, OUTPUT); //relay(pin 4) sebagai output
+  pinMode(pir, INPUT); //pir(pin 2) sebagai input
+  pinMode(ldr, INPUT); //ldr(pin A0) sebagai input
+  Serial.begin(9600); //inisialisasi serial dengan 9600 baud
 }
 
 void loop()
 {
-  digitalWrite(relay, LOW);
-  statuspir = digitalRead(pir);
-  statusldr = analogRead(ldr);
-  statusldr = map(statusldr, 1, 310, 1, 25000);
+  digitalWrite(relay, LOW); //set relay low(lampu mati)
+  statuspir = digitalRead(pir); //membaca data pir
+  statusldr = analogRead(ldr); //membaca data ldr
+  statusldr = map(statusldr, 1, 310, 1, 25000); //konversi pembacaan ldr ke lux
   
-  if(statuspir == HIGH && statusldr < 200){
-    Serial.println("Lampu Menyala");
-    digitalWrite(relay, HIGH);
-    delay(50000);
+  if(statuspir == HIGH && statusldr < 200){ //cek kondisi jika ada gerakan(statuspir == HIGH) dan cahaya kurang(statusldr < 200)
+    Serial.println("Lampu Menyala"); //output "Lampu Menyala" pada serial
+    digitalWrite(relay, HIGH); //set relay high(lampu menyala)
+    delay(300000); //delay 300 second/5 minute
   }
-  else{
-    Serial.println(statuspir);
-    Serial.println(statusldr);
+  else{ //jika kondisi diatas tidak terpenuhi
+    Serial.println(statuspir); //output nilai pir pada serial
+    Serial.println(statusldr); //output nilai ldr pada serial
   }
 }
