89339dfaa118ef89af549a8f358c377a5d755ae8
aderama2711 <ade.ramadha@gmail.com>
2021-03-25 23:01:31 +0700

    Fix

--- SmartLamp/SmartLamp.ino
+++ SmartLamp/SmartLamp.ino
@@ -11,6 +11,7 @@
 #define pir 5 //inisialisasi pir sebagai 5(pin pir di D1)
 #define ldr A0 //inisialisasi ldr sebagai A0(pin dari ldr)
 int statusldr = 0; //inisialisasi variabel pembacaan ldr
+int statuspir = 0; //inisialisasi variabel pembacaan ldr
 
 AntaresESP8266MQTT antares(ACCESSKEY);
 
@@ -29,24 +30,25 @@ void loop() {
 
   digitalWrite(relay, HIGH); //set relay high(lampu mati)
   statusldr = analogRead(ldr); //membaca data ldr
-  
+  statuspir = digitalRead(pir); //membaca data pir
   //dibawah ini code untuk konversi data ldr ke lux
   float Vout = statusldr * (3.3 / 675); //Vout = inputLDR * (Vin / ADC_maks)
   float RLDR = (1000 * (3.3 - Vout)) / Vout; //RLDR = (Resistor * (Vin - Vout)) / Vout
   statusldr = 500 / (RLDR*0.001); //Lux = 500 / (RLDR * 0.001)
-  antares.add("lux", statusldr);
-  antares.add("pir", digitalRead(pir));
-  antares.publish(projectName, deviceName);
   
-  if(digitalRead(pir) == HIGH && statusldr < 200){ //cek kondisi jika ada gerakan(statuspir == HIGH) dan cahaya kurang(statusldr < 200)
+  if(statuspir == HIGH && statusldr < 200){ //cek kondisi jika ada gerakan(statuspir == HIGH) dan cahaya kurang(statusldr < 200)
     Serial.printf("Lampu Menyala\n"); //output "Lampu Menyala" pada serial
     digitalWrite(relay, LOW); //set relay low(lampu menyala)
     delay(300000); //delay 300000 ms / 300 second / 5 minute
   }
   else{ //jika kondisi diatas tidak terpenuhi
-    Serial.printf("Status PIR : %d\n",digitalRead(pir)); //output nilai pir pada serial
+    Serial.printf("Status PIR : %d\n",statuspir); //output nilai pir pada serial
     Serial.printf("Status LDR : %d Lux\n",statusldr); //output nilai ldr pada serial
   }
 
+  antares.add("lux", statusldr);
+  antares.add("pir", statuspir);
+  antares.publish(projectName, deviceName);
+  
   delay(1000); //memberi jeda antar pembacaan sensor
 }
